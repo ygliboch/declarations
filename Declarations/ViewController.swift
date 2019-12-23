2e18322f5beff96800b710b8360ffe5439a4ceef
ygliboch <yaroslavah0203@gmail.com>
2019-12-23 17:56:01 +0200

    start

---
+++ Declarations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Declarations
+//
+//  Created by Yaroslava Hlibochko on 23.12.2019.
+//  Copyright © 2019 Yaroslava Hlibochko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
